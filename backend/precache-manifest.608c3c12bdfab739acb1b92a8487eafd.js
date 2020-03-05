418b1b611714f58c39ec0de4d8e0eb01ceba656d
Abhishek Singh <street17cafe@gmail.com>
2020-03-05 08:45:54 +0530

    Updated frontend and backend

---
+++ backend/precache-manifest.608c3c12bdfab739acb1b92a8487eafd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef5c3bb1400af71fdc4d037159b426d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08bca5d5eb48c9e7fa6",
+    "url": "/static/js/2.f272222a.chunk.js"
+  },
+  {
+    "revision": "67ca372235b05360542d",
+    "url": "/static/js/main.d577592c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
